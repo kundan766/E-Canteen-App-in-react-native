b83055cf1a17f15b9cd9a04cf7eaf5b7faffa8cb
kundan766 <ckundan198@gmail.com>
2024-11-17 21:16:04 +0530

    fix the code

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,37 +1,4 @@
-// // app/navigation/AppNavigator.tsx
-// import React,{useState} from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import AdminScreen from "../screens/AdminScreen"
-// import StudentScreen from '../screens/StudentScreen';
-// import OrderHistoryScreen from '../screens/OrderHistoryScreen';
-// import { Button, View } from 'react-native';
 
-// const Stack = createNativeStackNavigator();
-
-
-// const AppNavigator = () => {
-//     const [isAdmin, setIsAdmin] = useState(true); // Default to Admin view
-//   return (
-//     <NavigationContainer>
-//         <View style={{ flexDirection: 'row', justifyContent: 'center', margin: 10 }}>
-//         <Button title="Switch to Admin" onPress={() => setIsAdmin(true)} />
-//         <Button title="Switch to Student" onPress={() => setIsAdmin(false)} />
-//       </View>
-//       <Stack.Navigator initialRouteName={isAdmin ? "Admin" : "Student"}>
-//         <Stack.Screen name="Admin" component={AdminScreen} />
-//         <Stack.Screen name="Student" component={StudentScreen} />
-//         <Stack.Screen name="OrderHistory" component={OrderHistoryScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// };
-
-// export default AppNavigator;
-
-
-// new code 
-// app/navigation/AppNavigator.tsx
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -43,7 +10,7 @@ import { Button, View } from 'react-native';
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
-  const [isAdmin, setIsAdmin] = useState(true); // Default to Admin view
+  const [isAdmin, setIsAdmin] = useState(true); 
 
   return (
     <NavigationContainer>
